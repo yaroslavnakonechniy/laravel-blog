d5c3f191e03cd531a6af1c33c5b029fc7dc03f73
yaroslavn <yaroslav.n@open-tag.com>
2023-11-13 10:39:49 +0200

    crud Post: added new routes for PostController.

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', 'PostController@index')->namme('posts.index');
+Route::get('/posts/create', 'PostController@create')->namme('posts.create');
+Route::post('/posts', 'PostController@store')->namme('posts.store');
+Route::get('/posts/{post}', 'PostController@show')->namme('posts.show');
+Route::get('/posts/{post}/edit', 'PostController@edit')->namme('posts.edit');
+Route::patch('/posts/{post}', 'PostController@update')->namme('posts.update');
+Route::get('/posts/{post}', 'PostController@destroy')->namme('posts.destroy');
