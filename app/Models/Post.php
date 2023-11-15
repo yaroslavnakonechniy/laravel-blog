900519f3eddce169867f58984bdff439d9b5c94c
yaroslavn <yaroslav.n@open-tag.com>
2023-11-15 21:50:10 +0200

    oneToManyPostCategory: created relations between post and category.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Category;
+
 
 class Post extends Model
 {
     use HasFactory;
+
+    public function category(){
+        
+        return $this->belongsTo(Category::class);
+
+    }
 }
