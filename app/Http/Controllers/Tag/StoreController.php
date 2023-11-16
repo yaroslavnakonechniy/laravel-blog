9d15fd24c923a23dd89d49384c211a132eab9ec6
yaroslavn <yaroslav.n@open-tag.com>
2023-11-16 16:02:05 +0200

    services: created Service class for TagController.

--- app/Http/Controllers/Tag/StoreController.php
+++ app/Http/Controllers/Tag/StoreController.php
@@ -4,16 +4,14 @@ namespace App\Http\Controllers\Tag;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Tag;
 use App\Http\Requests\TagRequest;
+use App\Http\Controllers\Tag\BaseController;
 
-class StoreController extends Controller
+class StoreController extends BaseController
 {
     public function __invoke(TagRequest $req){
-        $tag = new Tag();
 
-        $tag->title = $req->input('title');
-        $tag->save();
+        $this->service->store($req);
 
         return redirect()->route('tag.index')->with('success', 'Новий тег було додано успішно');
     }
--- app/Http/Controllers/Tag/UpdateController.php
+++ app/Http/Controllers/Tag/UpdateController.php
@@ -6,16 +6,14 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Http\Requests\TagRequest;
 use App\Models\Tag;
+use App\Http\Controllers\Tag\BaseController;
 
-class UpdateController extends Controller
+class UpdateController extends BaseController
 {
     public function __invoke(TagRequest $req, $tag_id){
-        $tag = Tag::find($tag_id);
 
-        $tag->title = $req->input('title');
-        $tag->update();
+        $this->service->update($req, $tag_id);
 
         return redirect()->route('tag.index')->with('success', 'Тег було оновлено успішно');
     }
-
 }
