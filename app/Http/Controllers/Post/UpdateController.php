0f82261f2b3741a475a0a6c0cfa31afb844fc160
yaroslavn <yaroslav.n@open-tag.com>
2023-11-16 15:15:25 +0200

    services: created Service class for PostController.

--- app/Http/Controllers/Post/UpdateController.php
+++ app/Http/Controllers/Post/UpdateController.php
@@ -8,23 +8,13 @@ use App\Http\Requests\PostRequest;
 use App\Http\Requests\TagRequest;
 use App\Models\Post;
 use App\Models\Tag;
+use App\Http\Controllers\Post\BaseController;
 
-class UpdateController extends Controller
+class UpdateController extends BaseController
 {
     public function __invoke(PostRequest $request, TagRequest $tag_req, $post_id){
-        $post = Post::find($post_id);
-        $tag = new Tag();
 
-        $post->title = $request->input('title');
-        $post->content = $request->input('content');
-        $post->category_id = $request->input('category_id');
-
-        $tag->tag_id = $tag_req->input('tag_id');
-        $tags=$tag['tag_id'];
-        
-        $post->save();
-        
-        $post->tags()->sync($tags);
+        $this->service->update($request, $tag_req, $post_id);
         
         return redirect()->route('posts.index')->with('success', 'Дані булo оновлено успішно');
 
---
+++ app/Services/Post/Service.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Services\Post;
+
+use App\Models\Post; 
+use App\Models\Tag;
+use App\Http\Requests\PostRequest;
+use App\Http\Requests\TagRequest;
+
+class Service {
+
+    public function store( $request, $tag_req){
+        
+        $post = new Post();
+        $tag = new Tag();
+
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->category_id = $request->input('category_id');
+
+        $tag->tag_id = $tag_req->input('tag_id');
+        $tags=$tag['tag_id'];
+        
+        $post->save();
+        
+        $post->tags()->attach($tags);
+
+    }
+
+    public function update($request, $tag_req, $post_id){
+        
+        $post = Post::find($post_id);
+        $tag = new Tag();
+
+        $post->title = $request->input('title');
+        $post->content = $request->input('content');
+        $post->category_id = $request->input('category_id');
+
+        $tag->tag_id = $tag_req->input('tag_id');
+        $tags=$tag['tag_id'];
+        
+        $post->save();
+        
+        $post->tags()->sync($tags);
+    }
+}
