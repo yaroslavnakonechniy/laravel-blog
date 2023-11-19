9d3304242658bce32382105d67ac5ff1231ed06d
yaroslavn <yaroslav.n@open-tag.com>
2023-11-19 13:01:34 +0200

    admin LTE: created new voiews for admin panel.

---
+++ resources/views/includes/admin/sidebar.blade.php
@@ -0,0 +1,20 @@
+<nav class="mt-2">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+          <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+          
+          <li class="nav-item">
+            <a href="{{route('admin.posts.index')}}" class="nav-link">
+              <i class="nav-icon far fa-circle text-warning"></i>
+              <p>Posts</p>
+              <span class="badge badge-info right">  </span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon far fa-circle text-info"></i>
+              <p>Informational</p>
+            </a>
+          </li>
+        </ul>
+      </nav>
